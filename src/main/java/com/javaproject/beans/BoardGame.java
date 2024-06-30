7c0e1ad3618a0b8ef3d341a0479727db08c08a0c
nlakshmi7 <157840337+nlakshmi7@users.noreply.github.com>
2024-06-30 01:43:11 -0500

    Update BoardGame.java

--- src/main/java/com/javaproject/beans/BoardGame.java
+++ src/main/java/com/javaproject/beans/BoardGame.java
@@ -7,4 +7,13 @@ import lombok.Data;
 @Data
 public class BoardGame {
 
+    private Long id;
+    private String name;
+    private int level;
+    private int minPlayers;
+    private String maxPlayers;
+    private String gameType;
+
+    private List<Review> reviews;
+
 }
